9395c6584fbf89b68b1accbd991bc86c5c458192
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-04-13 23:51:03 +0530

    23:50 13 Apr

---
+++ python/student.py
@@ -0,0 +1,9 @@
+student = {"name": "anjali", "age": 22, "id": 77}
+keys = []
+values = []
+for k in student:
+    keys.append(k)
+print(keys)
+for v in student:
+    values.append(student[v])
+print(values)
