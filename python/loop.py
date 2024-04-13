03fff4d7865c4c3f3c0ef907f16de3bf1e5a0037
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-04-14 01:13:11 +0530

    01:13 14 Apr

--- python/loop.py
+++ python/loop.py
@@ -1,6 +1,16 @@
 """for i in range(1,20,2):
     print(i)"""
+
+"""
 sum=0
 for i in range(1,6):
     sum+=i
-print(sum)    
+print(sum)"""
+
+list=["rakesh","hero","KKR","dad","Thiruvanathapuram"]
+max=0
+for i in list:
+    if(len(i)>max):
+        max=len(i)
+
+print(max)
