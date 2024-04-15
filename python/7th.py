59c21ee28333177f3b59e0e3515334d289281a9a
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-04-15 12:02:29 +0530

    12:02 15 Apr

---
+++ python/7th.py
@@ -0,0 +1,8 @@
+count=0
+while count<3:
+    print ("il")
+    count+=1
+    if count==2:
+        break
+else:
+    print("el")    
---
+++ python/8th.py
@@ -0,0 +1,7 @@
+for i in range(3):
+    for j in range(2):
+        if j==1:
+            break
+        print(i,j)
+else:
+    print("end")        
